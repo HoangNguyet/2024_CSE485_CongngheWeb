b13bbbfaa4630bc61f86504b51b0a0bee7b3523b
SaiThiLanHuong <lanhuong2k3sl@gmail.com>
2024-02-20 15:53:13 +0700

    hihi

---
+++ project_13/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Danh sách khóa học</title>
+    <style>
+        
+    </style>
+</head>
+<body>
+    <?php
+    $courses = [ 
+        [
+            'title' => 'LẬP TRÌNH VIÊN QUỐC TẾ',
+            'description' => 'Chương trình đào tạo chính thức tại Việt Nam từ Aptech Gobal. Phát triển nghề nghiệp sinh siên IT như một lập trình viên quốc tế.',
+            'fee' => '15.000.000 VND',
+            'start_date' =>'2/2/24',
+            'duration' => '2 - 2.5 năm'
+        ],
+        [
+            'title' => 'LẬP TRÌNH WEB FULLSTACK',
+            'description' => 'Chương trình đào tạo chính thức tại Việt Nam từ Aptech Gobal. Phát triển nghề nghiệp sinh siên IT như một lập trình viên quốc tế.',
+            'fee' => '15 % học phí',
+            'start_date' =>'2/2/24',
+            'duration' => '6 tháng'
+        ],
+        [
+            'title' => 'LẬP TRÌNH JAVA FULLSTACK',
+            'description' => 'Chương trình đào tạo chính thức tại Việt Nam từ Aptech Gobal. Phát triển nghề nghiệp sinh siên IT như một lập trình viên quốc tế.',
+            'fee' => '15 % học phí',
+            'start_date' =>'2/2/24',
+            'duration' => '236 giờ' //?????
+        ],
+    ];
+    foreach($courses as $course) {
+        echo '<div class="course">';
+        echo '<h2>'.$course['title'].'</h2>';
+        echo '<p>'.$course['description'].'</p>';
+        echo '<p> Học phí: '.$course['fee'].'</p>';
+        echo '<p>Ngày khai giảng:'.$course['start_date'].'</p>';
+        echo '<p>Thời lượng:'.$course['duration'].'</p>';
+        echo '</div>';
+    }
+    ?>
+</body>
+</html>
---
+++ project_14/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    $contries = array(
+        'BẮC NINH',
+        'HÀ NỘI',
+        'THÁI NGUYÊN',
+        'BẮC GIANG',
+        'NAM ĐỊNH',
+        'THÁI BÌNH',
+        'HÀ NAM',
+        'VĨNH PHÚC',
+    );
+</body>
+</html>
