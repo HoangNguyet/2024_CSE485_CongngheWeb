449430c951e19be0e583f4e82ec9e68f90c4e154
HoangNguyet <nguyethoanghan@gmail.com>
2024-02-26 21:18:30 +0700

    update project

--- project22/update_profile.php
+++ project22/update_profile.php
@@ -1,1 +1,49 @@
+<!--Xử lý upload aảnh và thay avatar-->
 <?php
+// Kiểm tra xem form đã được submit chưa
+if($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+    // Kiểm tra tính hợp lệ và Update user information
+    $errors = [];
+    $user['name'] = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+    // Handle avatar upload
+    $allowedExtensions = ['jpg', 'jpeg', 'png'];
+    $maxsize = 1048576;
+    $targetDir = "uploads";
+    if(!empty($_FILES['avatar']['tmp_name']))
+    {
+        $fileInfo = pathinfo($_FILES['avatar']['name']);
+        if(!in_array($fileInfo['extension'], $allowedExtensions))
+        {
+            $errors[] = "Only JPG, PPEG, and PNG files are allowed.";
+        }else if($_FILES['avatar']['size'] > $maxsize)
+        {
+            $errors[] = "File size must be less than 1MB.";
+        }
+        else{
+            $fileName = uniqid() . "." . $fileInfo['extension'];
+            $targetFile = $targetDir . $fileName;
+            if(move_uploaded_file($_FILES['avatar']['tmp_name'], $targetFile)){
+                $user['avatar'] = $targetFile; // upload avatar URL in array
+            }
+            else{
+                $errors [] = "Failed to upload file";
+            }
+        }
+    }
+    // Uplaod profile
+    if(empty($errors))
+    {
+        //upload profile
+        echo "Profile update successfully";
+    }
+    else{
+        echo "Errors:";
+        foreach ($errors as $er)
+        {
+            echo "<br> - $er";
+        }
+    }
+}
+?>
--- project22/users.php
+++ project22/users.php
@@ -18,24 +18,24 @@
             'avatar' => 'assets\img\avatar.jpg'
         ];
     ?>
-<form class="form" action="update_profile.php" method="post" enctype="multipart/form-data">
-    <form action="update_profile.php" method="post" enctype="multipart/form-data">
-        <h2>Profile Information</h2>
-        <label for="name">Name:</label>
+    <!--// Xây dựng form-->
+    <form class="form" action="update_profile.php" method="post" enctype="multipart/form-data">
+    <h2>Profile Information</h2>
+        <label for="name">Name</label>
         <br>
         <input class="input" type="text" name="name" value="<?php echo $user['name']; ?>" required>
         <br>
-        <label for="email">Email:</label>
+        <label for="email">Email</label>
         <br>
         <input class="input" type="email" name="email" value="<?php echo $user['email']; ?>" disabled>
         <br>
-        <label for="avatar">Avatar:</label>
+        <label for="avatar">Avatar</label>
         <br>
         <input class="input" type="file" name="avatar" accept="image/*">
         <br>
         <button class="button" type="submit">Update Profile</button>
     </form>
-</form>
 </div>
+
 </body>
 </html>
