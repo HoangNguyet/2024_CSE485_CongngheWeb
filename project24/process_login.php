ca88fc53b33edfb331f82c7aa944ec53f1bcee85
HoangNguyet <nguyethoanghan@gmail.com>
2024-02-27 00:14:03 +0700

    update project

---
+++ project24/process_login.php
@@ -0,0 +1,28 @@
+<?php
+    $users = [
+    [
+        "username" => "Hoàng Ánh Nguyệt",
+        "password" => password_hash("31012003", PASSWORD_DEFAULT),
+        "name" => "Nguyệt",
+        "email" => "nguyethoanghan@gmail.com"
+    ],
+    [
+        "username" => "Nguyễn Thị Thanh Mai chó",
+        "password" => password_hash("khongbiet", PASSWORD_DEFAULT),
+        "name" => "Mai cho",
+        "email" => "maingu@gmail.com"
+    ],
+    [
+        "username" => "Hoàng Minh Nghĩa",
+        "password" => password_hash("14012016", PASSWORD_DEFAULT),
+        "name" => "Nghĩa",
+        "email" => "hoangminhnghia@gmail.com"
+    ],
+    [
+        "username" => "Hoàng Minh Hải",
+        "password" => password_hash("22092007", PASSWORD_DEFAULT),
+        "name" => "Hải",
+        "email" => "hoangminhhai@gmail.com"
+    ],
+];
+?>
