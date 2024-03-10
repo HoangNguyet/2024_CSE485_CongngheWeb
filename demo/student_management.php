9fa598feec6778ed26f123c3df2e33ff7b920395
SaiThiLanHuong <157559269+SaiThiLanHuong@users.noreply.github.com>
2024-03-10 13:41:22 +0700

    Delete demo directory

--- demo/student_management.php
+++
@@ -1,5 +0,0 @@
-<form action="process_login.php" method="post">
-    <input type="text" name="username" placeholder="Username">
-    <input type="password" name="password" placeholder="Password">
-    <button type="submit">Login</button>
-</form>
